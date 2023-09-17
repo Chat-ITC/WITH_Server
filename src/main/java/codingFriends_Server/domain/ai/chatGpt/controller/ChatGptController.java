1b7c26ecae34cea74d5feced865a3a3898f0448c
hong <kevin0928@naver.com>
2023-09-17 12:44:38 +0900

    fix: commit 복구

--- src/main/java/codingFriends_Server/domain/ai/chatGpt/controller/ChatGptController.java
+++ src/main/java/codingFriends_Server/domain/ai/chatGpt/controller/ChatGptController.java
@@ -19,9 +19,9 @@ public class ChatGptController {
     private final ChatGptService communityService;
 
     @PostMapping("")
-    public ResponseEntity<ChatResponse> ChatGpt(@RequestBody String prompt) {
+    public ResponseEntity<String> ChatGpt(@RequestBody String prompt) {
 
-        ChatResponse chatResponse = communityService.askQuestion(prompt);
+        String chatResponse = communityService.askQuestion(prompt);
         System.out.println("chatResponse = " + chatResponse);
         return ResponseEntity.ok(chatResponse);
 
--- src/main/java/codingFriends_Server/domain/ai/chatGpt/service/ChatGptService.java
+++ src/main/java/codingFriends_Server/domain/ai/chatGpt/service/ChatGptService.java
@@ -1,5 +1,6 @@
 package codingFriends_Server.domain.ai.chatGpt.service;
 
+import codingFriends_Server.domain.ai.chatGpt.dto.Message;
 import codingFriends_Server.domain.ai.chatGpt.request.ChatRequest;
 import codingFriends_Server.domain.ai.chatGpt.response.ChatResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,17 +12,20 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ChatGptService {
-    @Value("${openai.model}")
+    @Value("${spring.openai.model}")
     private String model;
 
-    @Value("${openai.api.url}")
+    @Value("${spring.openai.api.url}")
     private String apiUrl;
 
-    @Value("${openai.api.api-key}")
+    @Value("${spring.openai.api.api-key}")
     private String openaiApiKey;
 
     private HttpEntity<ChatRequest> getHttpEntity(ChatRequest chatRequest) {
@@ -33,12 +37,18 @@ public class ChatGptService {
     }
 
     @Async
-    public ChatResponse askQuestion(String prompt) {
+    public String askQuestion(String prompt) {
         ChatRequest chatRequest = new ChatRequest(model, prompt);
         RestTemplate restTemplate = new RestTemplate();
         ChatResponse response = restTemplate.postForObject(apiUrl, getHttpEntity(chatRequest), ChatResponse.class);
-        return response;
+
+        List<ChatResponse.Choice> choiceList = response.getChoices();
+
+        List<String> messageTexts = choiceList.stream().map(ChatResponse.Choice::getMessage)
+                .map(Message::getContent)
+                .collect(Collectors.toList());
+        System.out.println("response = " + response);
+        return String.join("\n", messageTexts);
     }
 }
-
 
