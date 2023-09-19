34eab0b1675f82043349017154c0d71d08cffb84
hong <kevin0928@naver.com>
2023-09-19 19:15:55 +0900

    fix: language 저장할 때 중복 검사 추가

--- src/main/java/codingFriends_Server/domain/question/controller/LanguageController.java
+++ src/main/java/codingFriends_Server/domain/question/controller/LanguageController.java
@@ -14,9 +14,6 @@ public class LanguageController {
 
     @PostMapping("/language/save")
     public ResponseEntity<?> saveLanguage(@RequestParam("type") String type) {
-        if (type == null) {
-            throw new CustomException(HttpStatus.NOT_FOUND, "type 문자열이 없습니다.");
-        }
         String result = languageService.save(type);
         return ResponseEntity.ok()
                 .body(result);
@@ -26,9 +23,6 @@ public class LanguageController {
     public ResponseEntity<?> updateLanguage(
             @RequestParam("id") Long id,
             @RequestParam("type") String type) {
-        if (id == null || type == null) {
-            throw new CustomException(HttpStatus.NOT_FOUND, "id 또는 문자열이 없습니다.");
-        }
         languageService.updateLanguage(id, type);
         return ResponseEntity.ok()
                 .body("Language 수정 완료");
@@ -36,9 +30,7 @@ public class LanguageController {
     @DeleteMapping("/language/delete")
     public ResponseEntity<?> deleteLanguage(
             @RequestParam("id") Long id) {
-        if (id == null) {
-            throw new CustomException(HttpStatus.NOT_FOUND, "id가 없습니다.");
-        }
+
         languageService.deleteLanguage(id);
         return ResponseEntity.ok()
                 .body("Language 삭제 완료");
